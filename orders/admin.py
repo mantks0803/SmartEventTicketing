916646a18d6d8d3ae6bad340dd99fb8d8e759eb9
man_tks <dephucau@gmail.com>
2026-07-29 15:46:44 +0700

    payment-update

--- orders/admin.py
+++ orders/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from orders.models import Order, Ticket
+from orders.models import Order, Ticket, Payment
 
 admin.site.register(Order)
 admin.site.register(Ticket)
+admin.site.register(Payment)
