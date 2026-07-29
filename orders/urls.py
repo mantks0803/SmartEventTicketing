a9b4912f042ee0f1a48abe410e6910a3339a6ec8
man_tks <dephucau@gmail.com>
2026-07-29 12:45:26 +0700

    paysos-payment-gateway added

--- orders/urls.py
+++ orders/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
-from orders.views import HoldSeatsView, CustomerOrderListView, OrderDetailView, CancelOrderView
+from orders.views import (
+    HoldSeatsView, CustomerOrderListView, OrderDetailView, 
+    CancelOrderView, CreatePayOSPaymentView, PayOSWebhookView
+)
 
 urlpatterns = [
     path('hold/', HoldSeatsView.as_view(), name='hold_seats'),
     path('my-orders/', CustomerOrderListView.as_view(), name='customer_orders'),
     path('<int:pk>/', OrderDetailView.as_view(), name='order_detail'),
     path('<int:pk>/cancel/', CancelOrderView.as_view(), name='cancel_order'),
+    
+    path('<int:order_id>/payos-link/', CreatePayOSPaymentView.as_view(), name='create_payos_link'),
+    path('webhook/payos/', PayOSWebhookView.as_view(), name='payos_webhook'),
 ]
