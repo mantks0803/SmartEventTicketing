f11e7bf6a2771a4330040ad603ebbb7812d5cda5
man_tks <dephucau@gmail.com>
2026-08-02 18:21:59 +0700

    updated front end basic

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,10 +1,16 @@
-<script setup></script>
-
 <template>
-  <h1>You did it!</h1>
-  <p>
-    hello
-  </p>
+  <div class="d-flex flex-column min-vh-100">
+    <AppNavbar />
+
+    <main class="flex-grow-1">
+      <router-view />
+    </main>
+
+    <AppFooter />
+  </div>
 </template>
 
-<style scoped></style>
+<script setup>
+import AppNavbar from '@/components/AppNavbar.vue'
+import AppFooter from '@/components/AppFooter.vue'
+</script>
