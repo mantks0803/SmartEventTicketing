1d12252a10f57f4ccebb4c3a5b0811985e0ee5ec
man_tks <dephucau@gmail.com>
2026-09-01 22:03:22 +0700

    demo

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios'; 
-
+import.meta.env.VITE_API_URL || 'http://127.0.0.1:8000/api/'
 
 const apiClient = axios.create({
    baseURL: import.meta.env.VITE_API_URL || 'http://127.0.0.1:8000/api/',
@@ -26,7 +26,6 @@ apiClient.interceptors.response.use(
   (response) => response,
   (error) => {
     if (error.response && error.response.status === 401) {
-      // Token không hợp lệ hoặc hết hạn, xóa token và thông tin người dùng khỏi localStorage
       localStorage.removeItem('access_token')
       localStorage.removeItem('user_info')
     }
